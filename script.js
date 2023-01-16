fd640c0eb7d44aa7052806bbe7802467efee607d
Dendi13 <98680907+Dendi13@users.noreply.github.com>
2023-01-16 18:01:37 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -3,16 +3,44 @@ let cells = document.querySelectorAll('#field td');
 let i = 0;
 function start(cells) {
     for (let cell of cells) {
-        cell.addEventListener('click', function cellsing() {
+        cell.addEventListener('click', function step() {
             if (i % 2 == 0) {
                 cell.textContent = 'X'
             } else {
                 cell.textContent = 'O'
             }
             i++;
-            this.removeEventListener('click', cellsing)
+            this.removeEventListener('click', step);
+            if (isWinner(cells) == true) {
+                console.log(`Победитель ${this.textContent}`);
+            }
         })
 
     }
 }
+
 start(cells);
+
+function isWinner(cells) {
+    let combs = [
+        [0, 1, 2], //0
+        [3, 4, 5], //1
+        [6, 7, 8], //2
+        [0, 3, 6], //3
+        [1, 4, 7], //4
+        [2, 5, 8], //5
+        [0, 4, 8], //6
+        [2, 4, 6]  //7
+    ]
+    for (let comb of combs) {
+        if (cells[comb[0]].textContent == cells[comb[1]].textContent && cells[comb[1]].textContent ==
+            cells[comb[2]].textContent && cells[comb[0]].textContent != '') {
+            return true;
+        }
+    }
+    return false;
+
+}
+
+
+
